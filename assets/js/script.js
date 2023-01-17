890662131d06fbb22cfbf11f4818d66ba8d94f7d
DESKTOP-3HA26UM\ASUS <givesachin@gmail.com>
2023-01-17 19:04:45 +0530

    clear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var app = angular.module('pepnApp', []);
 
 app.controller('controller', function($scope, $http) {
+    $scope.firstName= "John";
+    $scope.lastName= "Doe";
 
     $scope.submitData = function (form) {
+        console.log('test');
 
         var formData = new FormData();
 
@@ -10,9 +13,12 @@ app.controller('controller', function($scope, $http) {
         formData.append('email', $scope.email);
         formData.append('message', $scope.message);
 
+        const headers = new Headers;
+        headers.append('Access-Control-Allow-Origin', '*');
+
         $("#overlay").fadeIn();
         $http.post('https://unique-func.azurewebsites.net/api/sendgrid_email?code=GcJwU7tew1BcDUAE4uZ7bgVZh83kH9Ca70mOyJeaJ94HKhJ07WPvsg==',
-            formData
+            formData, { headers: headers }
         )
             .then(function (response)
             {
